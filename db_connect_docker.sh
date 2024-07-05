301708a5df938526644fdaf2dfdde047a0e5debb
net-qwerty <oleg.oleg-ivanov2014@yandex.ru>
2024-07-05 10:05:22 +0300

    add postgres in docker  compose

---
+++ db_connect_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker exec -it student-bot-pg_db-1 psql -U student_bot
---
+++ db_connect_psql.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PGPASSWORD="student_bot"
+
+psql -h 127.0.0.1 -U student_bot -d student_bot
