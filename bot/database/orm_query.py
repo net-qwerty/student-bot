0bd2efac8a0c046b342a8c6e4bb5db961522b671
ksyusha25 <ksyushasoboleva2001@yandex.ru>
2024-07-06 21:57:53 +0300

    crud. добавлено поле "номер семестра" в Семестре

--- bot/database/orm_query.py
+++ bot/database/orm_query.py
@@ -1,43 +1,158 @@
-# from sqlalchemy import select, update, delete
-# from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, update, delete
+from sqlalchemy.ext.asyncio import AsyncSession
 
-# from database.models import Product
-
-
-# async def orm_add_product(session: AsyncSession, data: dict):
-#     obj = Product(
-#         name=data["name"],
-#         description=data["description"],
-#         price=float(data["price"]),
-#         image=data["image"],
-#     )
-#     session.add(obj)
-#     await session.commit()
+from database.models import Post, Subject, Semestr, Group
 
 
-# async def orm_get_products(session: AsyncSession):
-#     query = select(Product)
-#     result = await session.execute(query)
-#     return result.scalars().all()
+# ADD
+async def orm_add_post(session: AsyncSession, data: dict):
+    obj = Post(
+        text=data["text"],
+        deadline=data["deadline"],
+        subject_id=data["subject_id"],
+        subject=data["subject"],
+        material=data["material"],
+    )
+    session.add(obj)
+    await session.commit()
+
+async def orm_add_subject(session: AsyncSession, data: dict):
+    obj = Subject(
+        name=data["name"],
+        semestr_id=data["semestr_id"],
+        semestr=data["semestr"],
+    )
+    session.add(obj)
+    await session.commit()
+
+async def orm_add_semestr(session: AsyncSession, data: dict):
+    obj = Semestr(
+        number=data["number"],
+        subjects=data["subjects"],
+        group_id=data["group_id"],
+        group=data["group"],
+    )
+    session.add(obj)
+    await session.commit()
+
+async def orm_add_group(session: AsyncSession, data: dict):
+    obj = Group(
+        name=data["name"],
+        codeName=data["codeName"],
+        headmanID=data["headmanID"],
+        notificationInterval=data["notificationInterval"],
+    )
+    session.add(obj)
+    await session.commit()
 
 
-# async def orm_get_product(session: AsyncSession, product_id: int):
-#     query = select(Product).where(Product.id == product_id)
-#     result = await session.execute(query)
-#     return result.scalar()
+
+# ADD ALL
+async def orm_get_post(session: AsyncSession):
+    query = select(Post)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+async def orm_get_subject(session: AsyncSession):
+    query = select(Subject)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+async def orm_get_semestr(session: AsyncSession):
+    query = select(Semestr)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+async def orm_get_group(session: AsyncSession):
+    query = select(Group)
+    result = await session.execute(query)
+    return result.scalars().all()
 
 
-# async def orm_update_product(session: AsyncSession, product_id: int, data):
-#     query = update(Product).where(Product.id == product_id).values(
-#         name=data["name"],
-#         description=data["description"],
-#         price=float(data["price"]),
-#         image=data["image"],)
-#     await session.execute(query)
-#     await session.commit()
+
+# ADD BY ID
+async def orm_get_post(session: AsyncSession, post_id: int):
+    query = select(Post).where(Post.id == post_id)
+    result = await session.execute(query)
+    return result.scalar()
+
+async def orm_get_subject(session: AsyncSession, subject_id: int):
+    query = select(Subject).where(Subject.id == subject_id)
+    result = await session.execute(query)
+    return result.scalar()
+
+async def orm_get_semestr(session: AsyncSession, semestr_id: int):
+    query = select(Semestr).where(Semestr.id == semestr_id)
+    result = await session.execute(query)
+    return result.scalar()
+
+async def orm_get_group(session: AsyncSession, group_id: int):
+    query = select(Group).where(Group.id == group_id)
+    result = await session.execute(query)
+    return result.scalar()
 
 
-# async def orm_delete_product(session: AsyncSession, product_id: int):
-#     query = delete(Product).where(Product.id == product_id)
-#     await session.execute(query)
-#     await session.commit()
+
+# UPDATE
+async def orm_update_post(session: AsyncSession, post_id: int, data):
+    query = update(Post).where(Post.id == post_id).values(
+        text=data["text"],
+        deadline=data["deadline"],
+        subject_id=data["subject_id"],
+        subject=data["subject"],
+        material=data["material"],
+        )
+    await session.execute(query)
+    await session.commit()
+
+async def orm_update_subject(session: AsyncSession, subject_id: int, data):
+    query = update(Subject).where(Subject.id == subject_id).values(
+        name=data["name"],
+        semestr_id=data["semestr_id"],
+        semestr=data["semestr"],
+        )
+    await session.execute(query)
+    await session.commit()
+
+async def orm_update_semestr(session: AsyncSession, semestr_id: int, data):
+    query = update(Semestr).where(Semestr.id == semestr_id).values(
+        number=data["number"],
+        subjects=data["subjects"],
+        group_id=data["group_id"],
+        group=data["group"],
+        )
+    await session.execute(query)
+    await session.commit()
+
+async def orm_update_group(session: AsyncSession, group_id: int, data):
+    query = update(Group).where(Group.id == group_id).values(
+        name=data["name"],
+        codeName=data["codeName"],
+        headmanID=data["headmanID"],
+        notificationInterval=data["notificationInterval"],
+        )
+    await session.execute(query)
+    await session.commit()
+
+
+
+# DELETE BY ID
+async def orm_delete_post(session: AsyncSession, post_id: int):
+    query = delete(Post).where(Post.id == post_id)
+    await session.execute(query)
+    await session.commit()
+
+async def orm_delete_subject(session: AsyncSession, subject_id: int):
+    query = delete(Subject).where(Subject.id == subject_id)
+    await session.execute(query)
+    await session.commit()
+
+async def orm_delete_semestr(session: AsyncSession, semestr_id: int):
+    query = delete(Semestr).where(Semestr.id == semestr_id)
+    await session.execute(query)
+    await session.commit()
+
+async def orm_delete_group(session: AsyncSession, group_id: int):
+    query = delete(Group).where(Group.id == group_id)
+    await session.execute(query)
+    await session.commit()
